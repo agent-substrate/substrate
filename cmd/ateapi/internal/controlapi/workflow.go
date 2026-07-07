49471cfccbe2909aed42f15c2fb457dd8e0198b6
msau42 <msau@google.com>
2026-07-07 23:39:30 +0000

    ateapi handling of external volumes.
    Adds external volumes to the Actor object and integrates
    control plane Actor workflows with the corresponding
    volume operations:
    
    CreateActor -> CreateVolume
    DeleteActor -> DeleteVolume
    ResumeActor -> AttachVolume
    Pause/Suspend Actor -> DetachVolume

--- cmd/ateapi/internal/controlapi/workflow.go
+++ cmd/ateapi/internal/controlapi/workflow.go
@@ -182,6 +182,7 @@ func (w *ActorWorkflow) ResumeActor(ctx context.Context, atespace, name string, 
 	steps := []WorkflowStep[*ResumeInput, *ResumeState]{
 		&LoadActorForResumeStep{store: w.store, actorTemplateLister: w.actorTemplateLister},
 		&AssignWorkerStep{store: w.store, workerCache: w.workerCache},
+		&AttachVolumesStep{store: w.store},
 		&CallAteletRestoreStep{store: w.store, dialer: w.dialer, kubeClient: w.kubeClient, secretCache: w.secretCache, workerPoolLister: w.workerPoolLister, sandboxConfigLister: w.sandboxConfigLister},
 		&FinalizeRunningStep{store: w.store},
 	}
@@ -213,6 +214,7 @@ func (w *ActorWorkflow) SuspendActor(ctx context.Context, atespace, name string)
 		&LoadActorForSuspendStep{store: w.store, actorTemplateLister: w.actorTemplateLister},
 		&MarkSuspendingStep{store: w.store},
 		&CallAteletSuspendStep{store: w.store, dialer: w.dialer},
+		&DetachVolumesStep{store: w.store},
 		&FinalizeSuspendedStep{store: w.store},
 	}
 
@@ -243,6 +245,7 @@ func (w *ActorWorkflow) PauseActor(ctx context.Context, atespace, name string) (
 		&LoadActorForPauseStep{store: w.store, actorTemplateLister: w.actorTemplateLister},
 		&MarkPausingStep{store: w.store},
 		&CallAteletPauseStep{store: w.store, dialer: w.dialer},
+		&DetachVolumesForPauseStep{store: w.store},
 		&FinalizePausedStep{store: w.store},
 	}
 
