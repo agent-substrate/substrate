3648b34e2803e984bf29614413b0e21653dc1243
Lior Lieberman <liorlieberman@google.com>
2026-07-31 13:47:29 -0700

    pin grpcio-tools, grpcio, protobuf for benchmarking

--- benchmarking/locust/common/ateapi_pb2_grpc.py
+++ benchmarking/locust/common/ateapi_pb2_grpc.py
@@ -19,7 +19,7 @@ import warnings
 
 from . import ateapi_pb2 as ateapi__pb2
 
-GRPC_GENERATED_VERSION = '1.82.1'
+GRPC_GENERATED_VERSION = '1.81.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
